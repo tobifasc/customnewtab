94b940c3553d58e7ee1d66adb95391649e417e40
Tobias Fasching <tobias.fasching@outlook.com>
2018-12-13 23:55:50 +0100

    Fix indentations and set background image

--- src/App.vue
+++ src/App.vue
@@ -9,20 +9,22 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    components: {
+        HelloWorld
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    height: 100%;
+    padding-top: 60px;
+    background-image: url("./assets/pexels-photo-1482777.jpeg");
 }
 </style>
