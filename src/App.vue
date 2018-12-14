1b8ee740145ac68918e4345ce85e8ad84a3dba06
Tobias Fasching <tobias.fasching@outlook.com>
2018-12-14 11:41:19 +0100

    Add clock component
    Use moment to set time in 1 second intervals

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="app">
         <Bookmarks class="container" />
+        <Clock />
         <div class="container"></div>
         <div class="container"></div>
         <div class="container"></div>
@@ -10,29 +11,30 @@
 
 <script>
 import Bookmarks from './components/Bookmarks.vue'
+import Clock from './components/Clock.vue'
 
 export default {
     name: 'app',
     components: {
-        Bookmarks
+        Bookmarks,
+        Clock
     }
 }
 </script>
 
 <style>
 #app {
-
     background: url("./assets/pexels-photo-1482777.jpeg") no-repeat center center fixed;
     background-size: cover;
 
     height: 100vh;
     display: grid;
     grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: repeat(4, 1fr);
+    grid-template-rows: 1fr 7em 1fr 1fr;
     grid-template-areas:
         "top top"
-        "topleft topright"
-        "midleft midright"
+        "status status"
+        "left right"
         "bottom bottom"
     ;
     grid-gap: 5% 5%;
@@ -46,4 +48,5 @@ export default {
     border-radius: 0.3em;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, .2);
 }
+
 </style>
---
+++ src/components/Clock.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="clock">
+        <span>{{ time }}</span>
+    </div>
+</template>
+
+<script>
+    let moment = require('moment');
+
+    export default {
+        name: "clock",
+        data () {
+            return {
+                time: ''
+            }
+        },
+        methods: {
+            updateTime () {
+                this.time = moment().format('LTS');
+            }
+        },
+        mounted () {
+            this.updateTime();
+            this.interval = setInterval(this.updateTime, 1000);
+        },
+        beforeDestroy () {
+            clearInterval(this.interval);
+        }
+    }
+</script>
+
+<style scoped>
+    .clock {
+        grid-area: status;
+        font-size: 4em;
+
+    }
+
+</style>
